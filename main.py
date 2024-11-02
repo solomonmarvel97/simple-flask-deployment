320589d21d9527c050bc3f737c0554c07a29304f
Marvelous Solomon <solomonmarvel@hotmail.com>
2024-11-02 21:12:56 +0100

    added name path param

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/<name>')
+def hello_world(name):
+  """Greets the user by name."""
+  return f'Hello World, {name}!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8080)
+  app.run(host='0.0.0.0', debug=True, port=8080)
